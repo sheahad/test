25f0b242ffbe2b94f59bc1b8331bc17c91f2dcd0
MD. KHALEED AL-AMRAN <sheahad@gmail.com>
2019-12-05 20:07:32 +0600

    added

---
+++ WebApp/WebApp/WebApp.BLL/BLL/ResultManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp.Model.Model;
+using WebApp.Repository.Repository;
+
+namespace WebApp.BLL.BLL
+{
+    public class ResultManager
+    {
+        ResultRepository _resultRepository = new ResultRepository();
+        public bool Add(Result result)
+        {
+            return _resultRepository.Add(result);
+        }
+    }
+}
